f59a84a59c493cbca16e148deab44f9ece1d4273
Sushi <susheendharvj@gmail.com>
2020-07-01 18:40:46 +0000

    login page done

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,3 +1,43 @@
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("login-div").style.display="none";
+      document.getElementById("user-div").style.display="block";
+      var user = firebase.auth().currentUser;
+
+      if(user!=null){
+          var email = user.email;
+      }
+
+      document.getElementById("user-para").innerHTML =  email;
+        
+
+
+
+    } else {
+      // No user is signed in.
+      document.getElementById("login-div").style.display="block";
+      document.getElementById("user-div").style.display="none";
+    }
+  });
+
+
+
 function login_alert() {
-    window.alert("it works!")
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPassword = document.getElementById("password_field").value;
+    
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+        window.alert("Error:" + errorMessage)
+      });
+}
+
+function logout() {
+    firebase.auth().signOut();
+    
 }
