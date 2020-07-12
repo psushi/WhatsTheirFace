5eee077986a12d401aded32275c30fec2b3cf108
Sushi <susheendharvj@gmail.com>
2020-07-12 14:01:50 +0000

    added image preview

--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -25,3 +25,41 @@ function upload() {
 
 
     }
+
+    const inpFile = document.getElementById("inpFile");
+    const previewContainer = document.getElementById("imagePreview");
+    const previewImage = previewContainer.querySelector(".image-preview__image");
+    const previewDefaultTex = previewContainer.querySelector(".image-preview__default-text");
+
+    inpFile.addEventListener("change" , function() {
+        const file = this.files[0];
+        if(file) {
+            const reader = new FileReader();
+            previewDefaultTex.style.display="none";
+            previewImage.style.display="block";
+
+            reader.addEventListener("load", function() {
+                console.log(this);
+                previewImage.setAttribute("src", this.result);
+
+            });
+
+            reader.readAsDataURL(file);
+        }
+
+        else{
+                previewDefaultTex.style.display=null;
+                previewImage.style.display=null;
+                previewImage.setAttribute("src","");
+
+            }
+
+        
+    })
+
+
+
+
+
+
+    
