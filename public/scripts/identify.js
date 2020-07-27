0a6c834c84941e8545a1173847d2586fc04c62a3
Sushi <susheendharvj@gmail.com>
2020-07-27 10:57:17 +0000

    useless

--- public/scripts/identify.js
+++ public/scripts/identify.js
@@ -1,302 +1,241 @@
-
-
 var loneGlobalVar = {};
 
-firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-        console.log("user signed-in");
-    }
-
-    else {
-        window.alert("no users wtf");
-
-    }
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log("user signed-in");
+  } else {
+    window.alert("no users wtf");
+  }
 });
 
-
 function upload() {
+  var image = document.getElementById("inpFile").files[0];
+  var uploadPrompt = document.getElementById("uploadDone");
+  var uploadImage = document.getElementById("uploadImage");
 
-    var image = document.getElementById("inpFile").files[0];
-    var uploadPrompt = document.getElementById("uploadDone");
-    var uploadImage = document.getElementById("uploadImage")
+  if (image) {
+    console.log("got here");
+    document.getElementById("progressBar").style.display = "inline-block";
+    var user = firebase.auth().currentUser;
+    var userId = user.uid;
 
-    if (image) {
-        console.log("got here")
-        document.getElementById("progressBar").style.display = "inline-block";
-        var user = firebase.auth().currentUser;
-        var userId = user.uid;
+    var imagename = image.name;
+    var storageRef = firebase
+      .storage()
+      .ref("images/" + userId + "/verify/" + imagename);
+    var progressBar = document.getElementById("progressBar");
+    var userInfo = document.getElementById("userInfo");
 
-        var imagename = image.name;
-        var storageRef = firebase.storage().ref('images/' + userId + "/verify/" + imagename);
-        var progressBar = document.getElementById("progressBar");
-        var userInfo = document.getElementById("userInfo");
+    var uploadTask = storageRef.put(image);
 
-        var uploadTask = storageRef.put(image);
+    uploadTask.on(
+      "state_changed",
+      function (snapshot) {
+        //observe state change events such as progress, pause , resume
+        //get task progress by including the number of bytes uploaded
 
-        uploadTask.on('state_changed', function (snapshot) {
-            //observe state change events such as progress, pause , resume
-            //get task progress by including the number of bytes uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("upload is" + progress + " done.");
 
-            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log("upload is" + progress + " done.");
+        progressBar.setAttribute("value", Math.round(progress));
+        if (progress === 100) {
+          console.log("inside");
+          progressBar.style.display = "none";
+          uploadPrompt.innerHTML = "Upload Finished!";
+          uploadPrompt.style.display = "block";
 
-            progressBar.setAttribute("value", Math.round(progress));
-            if (progress === 100) {
-                console.log("inside")
-                progressBar.style.display = "none";
-                uploadPrompt.innerHTML = "Upload Finished!";
-                uploadPrompt.style.display = "block";
-
-                setTimeout(function(){
-                    uploadPrompt.innerHTML = "Identifying....";
-                    getData();
-                    
-
-
-
-                },1500)
-
-
-            }
-
-
-        }, function (error) {
-            console.log(error.message);
-        }, function () {
-            //succesful upload
-            uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
-                //uploaded image url 
-                console.log(downloadURL);
-                loneGlobalVar.downloadURL = downloadURL;
-
-            })
-
+          setTimeout(function () {
+            uploadPrompt.innerHTML = "Identifying....";
+            getData();
+          }, 1500);
         }
-        )
-
-    }
-
-    else {
-        document.getElementById("uploadDone").innerHTML = "Please add an image for upload";
-        document.getElementById("uploadDone").style.display = "block";
-
-    }
+      },
+      function (error) {
+        console.log(error.message);
+      },
+      function () {
+        //succesful upload
+        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+          //uploaded image url
+          console.log(downloadURL);
+          loneGlobalVar.downloadURL = downloadURL;
+        });
+      }
+    );
+  } else {
+    document.getElementById("uploadDone").innerHTML =
+      "Please add an image for upload";
+    document.getElementById("uploadDone").style.display = "block";
+  }
 }
 
 const inpFile = document.getElementById("inpFile");
 const previewContainer = document.getElementById("imagePreview");
 const previewImage = previewContainer.querySelector(".image-preview__image");
-const previewDefaultTex = previewContainer.querySelector(".image-preview__default-text");
+const previewDefaultTex = previewContainer.querySelector(
+  ".image-preview__default-text"
+);
 
 inpFile.addEventListener("change", function () {
+  document.getElementById("uploadDone").style.display = "none";
+  document.getElementById("progressBar").setAttribute("value", "0");
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    previewDefaultTex.style.display = "none";
+    previewImage.style.display = "block";
 
-    document.getElementById("uploadDone").style.display = "none";
-    document.getElementById("progressBar").setAttribute("value", "0");
-    const file = this.files[0];
-    if (file) {
-        const reader = new FileReader();
-        previewDefaultTex.style.display = "none";
-        previewImage.style.display = "block";
-
-        reader.addEventListener("load", function () {
-            console.log(this);
-            previewImage.setAttribute("src", this.result);
-
-        });
-
-        reader.readAsDataURL(file);
-    }
-
-    else {
-        previewDefaultTex.style.display = null;
-        previewImage.style.display = null;
-        previewImage.setAttribute("src", "");
-
-    }
-
-
-})
-
-
-function submitInfo() {
-
-    var name = document.getElementById("userName").value;
-    var relation = document.getElementById("userRelation").value;
-    var database = firebase.database();
-    userData = database.ref().child("users");
-    var user = firebase.auth().currentUser;
-    var userEmail = user.email;
-    var userID = user.uid;
-
-    console.log("works");
-    console.log(user);
-
-
-
-    if (name) {
-        console.log("got in");
-        console.log(typeof (userEmail));
-        userData.child(userID).push({
-            Name: name,
-            relationship: relation
-        });
-    }
-
-
-    setTimeout(function () {
-        document.getElementById("submitInfo").style.display = "none";
-        document.getElementById("afterSubmit").style.display = "block";
-    }, 1000)
-
-}
-
-
-
-function getData() {
-    var user = firebase.auth().currentUser;
-    var database = firebase.database();
-    var userRef = database.ref('users/' + user.uid);
-    loneGlobalVar.userKeys = [];
-    loneGlobalVar.userEmbeddings = [];
-    loneGlobalVar.downloadURLs = [];
-    loneGlobalVar.info1 = [];
-
-    userRef.on('value', function (data) {
-        var info = data.val();
-        var keys = Object.keys(info);
-
-        for (var i = 0; i < keys.length; i++) {
-            var k = keys[i];
-            loneGlobalVar.userKeys.push(info[k].name);
-            loneGlobalVar.userEmbeddings.push(info[k].embedding);
-            loneGlobalVar.downloadURLs.push(info[k].downloadURL);
-            loneGlobalVar.info1.push(info[k].relationship);
-
-        }
-        console.log("got all user details!");
-
-        setTimeout(get_embed(loneGlobalVar.downloadURL),5000);
-        return;
-        
-        
-
+    reader.addEventListener("load", function () {
+      console.log(this);
+      previewImage.setAttribute("src", this.result);
     });
 
-    
-    
+    reader.readAsDataURL(file);
+  } else {
+    previewDefaultTex.style.display = null;
+    previewImage.style.display = null;
+    previewImage.setAttribute("src", "");
+  }
+});
 
+function submitInfo() {
+  var name = document.getElementById("userName").value;
+  var relation = document.getElementById("userRelation").value;
+  var database = firebase.database();
+  userData = database.ref().child("users");
+  var user = firebase.auth().currentUser;
+  var userEmail = user.email;
+  var userID = user.uid;
+
+  console.log("works");
+  console.log(user);
+
+  if (name) {
+    console.log("got in");
+    console.log(typeof userEmail);
+    userData.child(userID).push({
+      Name: name,
+      relationship: relation,
+    });
+  }
+
+  setTimeout(function () {
+    document.getElementById("submitInfo").style.display = "none";
+    document.getElementById("afterSubmit").style.display = "block";
+  }, 1000);
 }
 
+function getData() {
+  var user = firebase.auth().currentUser;
+  var database = firebase.database();
+  var userRef = database.ref("users/" + user.uid);
+  loneGlobalVar.userKeys = [];
+  loneGlobalVar.userEmbeddings = [];
+  loneGlobalVar.downloadURLs = [];
+  loneGlobalVar.info1 = [];
 
+  userRef.on("value", function (data) {
+    var info = data.val();
+    var keys = Object.keys(info);
+
+    for (var i = 0; i < keys.length; i++) {
+      var k = keys[i];
+      loneGlobalVar.userKeys.push(info[k].name);
+      loneGlobalVar.userEmbeddings.push(info[k].embedding);
+      loneGlobalVar.downloadURLs.push(info[k].downloadURL);
+      loneGlobalVar.info1.push(info[k].relationship);
+    }
+    console.log("got all user details!");
+
+    setTimeout(get_embed(loneGlobalVar.downloadURL), 5000);
+    return;
+  });
+}
 
 function get_embed(downloadURL) {
+  var xhr = new XMLHttpRequest();
+  var url =
+    "https://us-central1-first-cloud-function-282616.cloudfunctions.net/face_embedding";
+  xhr.open("POST", url, true);
 
-    var xhr = new XMLHttpRequest();
-    var url = "https://us-central1-first-cloud-function-282616.cloudfunctions.net/face_embedding";
-    xhr.open("POST", url, true);
-    
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 & xhr.status === 200) {
-            var reply = JSON.parse(xhr.responseText);
-            loneGlobalVar.new_embed = reply.embedding;
-            //console.log(loneGlobalVar.new_embed);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function () {
+    if ((xhr.readyState === 4) & (xhr.status === 200)) {
+      var reply = JSON.parse(xhr.responseText);
+      loneGlobalVar.new_embed = reply.embedding;
+      //console.log(loneGlobalVar.new_embed);
 
-            setTimeout(identify(),1000);
+      setTimeout(identify(), 1000);
+    }
+  };
 
-            
-
-        }
-    };
-
-    var data = JSON.stringify({ "downloadURL": downloadURL });
-    xhr.send(data);
-    return;
+  var data = JSON.stringify({ downloadURL: downloadURL });
+  xhr.send(data);
+  return;
 }
-
-
-
 
 function identify() {
+  var njnew_embed = nj.array(loneGlobalVar.new_embed);
+  var distances = [];
+  var sum = 0;
 
-    var njnew_embed = nj.array(loneGlobalVar.new_embed);
-    var distances = [];
-    var sum = 0;
+  for (let embeds of loneGlobalVar.userEmbeddings) {
+    console.log("embeds: " + embeds + "   new:   " + loneGlobalVar.new_embed);
+    embeds = nj.array(embeds);
+    let diff = embeds.subtract(njnew_embed);
+    diff = diff.pow(2);
+    distances.push(Math.sqrt(diff.sum()));
+  }
 
+  var index = indexOfMin(distances);
+  let njdist = nj.array(distances);
 
-    for (let embeds of loneGlobalVar.userEmbeddings) {
-        embeds = nj.array(embeds);
-        let diff = embeds.subtract(njnew_embed);
-        diff = diff.pow(2);
-        distances.push(Math.sqrt(diff.sum()));
-    }
+  //sigmoid -> threshold ----------------------------------------------------
+  var confidences = nj.sigmoid(njdist);
+  confidences = confidences.subtract(0.5);
+  var confidence = 1 - confidences.get(index);
 
-    var index = indexOfMin(distances);
-    let njdist = nj.array(distances);
+  // softmax -> threshold -----------------------------------------------------
 
-    //sigmoid -> threshold ----------------------------------------------------
-    var confidences = nj.sigmoid(njdist);
-    confidences = confidences.subtract(0.5);
-    var confidence = 1 - confidences.get(index);
-    
-
-
-    // softmax -> threshold -----------------------------------------------------
-
-    /*var confidences =nj.softmax(njdist);
+  /*var confidences =nj.softmax(njdist);
     var confidence = 1 - confidences.get(index);*/
 
+  console.log(loneGlobalVar.userKeys[index] + " confidence:" + confidence);
 
-    console.log(loneGlobalVar.userKeys[index]+" confidence:"+confidence);
+  previewImage.setAttribute("src", loneGlobalVar.downloadURLs[index]);
 
+  document.getElementById("uploadDone").innerHTML = "Match!";
+  document.getElementById("uploadImage").style.display = "none";
+  setTimeout(function () {
+    document.getElementById("uploadDone").style.display = "none";
+    document.getElementById("heading").innerHTML =
+      "It's " + loneGlobalVar.userKeys[index] + "!";
+    document.getElementById("identityName").innerHTML =
+      loneGlobalVar.userKeys[index];
+    document.getElementById("identityInfo-1").innerHTML =
+      loneGlobalVar.info1[index];
+    document.getElementById("confidence").innerHTML =
+      Math.round(confidence * 100) + "%";
+    document.getElementById("finalResult").style.display = "block";
+  }, 1000);
 
-    previewImage.setAttribute('src',loneGlobalVar.downloadURLs[index]);
-
-    document.getElementById("uploadDone").innerHTML ="Match!";
-    document.getElementById("uploadImage").style.display = "none";
-    setTimeout(function(){
-        document.getElementById("uploadDone").style.display="none";
-        document.getElementById("heading").innerHTML="It's "+loneGlobalVar.userKeys[index]+"!";
-        document.getElementById("identityName").innerHTML = loneGlobalVar.userKeys[index];
-        document.getElementById("identityInfo-1").innerHTML= loneGlobalVar.info1[index];
-        document.getElementById("confidence").innerHTML= Math.round(confidence*100)+"%";
-        document.getElementById("finalResult").style.display="block";
-
-
-    },1000)
-
-    return;
-
-
-
-
+  return;
 }
-
 
 function indexOfMin(arr) {
-    if (arr.length === 0) {
-        return -1;
+  if (arr.length === 0) {
+    return -1;
+  }
+
+  var min = arr[0];
+  var minIndex = 0;
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      minIndex = i;
+      min = arr[i];
     }
+  }
 
-    var min = arr[0];
-    var minIndex = 0;
-
-    for (var i = 1; i < arr.length; i++) {
-        if (arr[i] < min) {
-            minIndex = i;
-            min = arr[i];
-        }
-    }
-
-    return minIndex;
+  return minIndex;
 }
-
-
-
-
-
-
-
-
-
-
--- public/scripts/upload.js
+++ public/scripts/upload.js
@@ -1,201 +1,158 @@
-var singleGlobalVar ={};
+var singleGlobalVar = {};
 
-
-firebase.auth().onAuthStateChanged(user => {
-    if(user) {
-        console.log("user signed-in");
-    }
-
-    else {
-        window.alert("no users wth");
-        
-    }
-  });
-
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    console.log("user signed-in");
+  } else {
+    window.alert("no users wth");
+  }
+});
 
 function upload() {
-   
-    var image = document.getElementById("inpFile").files[0];
-    var uploadPrompt = document.getElementById("uploadDone");
-    var uploadImage= document.getElementById("uploadImage")
+  var image = document.getElementById("inpFile").files[0];
+  var uploadPrompt = document.getElementById("uploadDone");
+  var uploadImage = document.getElementById("uploadImage");
 
-    if(image){
-        console.log("got here")
-        document.getElementById("progressBar").style.display="inline-block";
-        var user = firebase.auth().currentUser;
-        var userId = user.uid;
-    
-        var imagename=image.name;
-        var storageRef= firebase.storage().ref('images/'+userId+"/"+imagename);
-        var progressBar = document.getElementById("progressBar");
-        var userInfo = document.getElementById("userInfo");
+  if (image) {
+    console.log("got here");
+    document.getElementById("progressBar").style.display = "inline-block";
+    var user = firebase.auth().currentUser;
+    var userId = user.uid;
 
-        var uploadTask = storageRef.put(image);
-        
-        uploadTask.on('state_changed',function(snapshot){
-            //observe state change events such as progress, pause , resume
-            //get task progress by including the number of bytes uploaded
-            
-            var progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
-            console.log("upload is" + progress + " done.");
-            
-            progressBar.setAttribute("value",Math.round(progress));
-            if(progress===100){
-                console.log("inside")
-                progressBar.style.display="none";
-                uploadPrompt.innerHTML ="Upload Finished!";
-                uploadPrompt.style.display = "block";
-                setTimeout(function (){
-                    uploadPrompt.innerHTML="Generating embedding..."
-                },500)
-                
-                
-            }
+    var imagename = image.name;
+    var storageRef = firebase
+      .storage()
+      .ref("images/" + userId + "/" + imagename);
+    var progressBar = document.getElementById("progressBar");
+    var userInfo = document.getElementById("userInfo");
 
+    var uploadTask = storageRef.put(image);
 
-        },function (error) {
-            console.log(error.message);
-        }, function () {
-            //succesful upload
-            uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL){
-                //uploaded image url 
-                console.log(downloadURL);
-                singleGlobalVar.downloadURL = downloadURL;
-                
-                //calling cloud function and saving it in singleGlobalVar--------------------------------
-                
-                var xhr = new XMLHttpRequest();
-                var url = "https://us-central1-first-cloud-function-282616.cloudfunctions.net/face_embedding";
-                xhr.open("POST",url,true);
-                xhr.setRequestHeader("Content-Type","application/json");
-                xhr.onreadystatechange = function() {
-                    if(xhr.readyState===4 & xhr.status===200){
-                        //var respond = JSON.parse(this.responseText);
-                        var embed = JSON.parse(xhr.responseText);
-                       
-            
-            
-                        singleGlobalVar.embedding = embed.embedding;
-                        
-                        
-                        setTimeout(function (){
-                            uploadImage.style.display="none";
-                            userInfo.style.display="block";
-                            uploadPrompt.style.display = "none";
-                            document.getElementById("heading").innerHTML= "Add Information related to Image ";
-                        },500);
-                        //singleGlobalVar.embedding = JSON.parse(xhr.responseText).embedding;
-                        //alert(singleGlobalVar.embedding[0]);
+    uploadTask.on(
+      "state_changed",
+      function (snapshot) {
+        //observe state change events such as progress, pause , resume
+        //get task progress by including the number of bytes uploaded
 
-                        
-                    }
-                };
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("upload is" + progress + " done.");
 
-                var data = JSON.stringify({"downloadURL":singleGlobalVar.downloadURL});
-                xhr.send(data);
-
-
-
-
-
-
-
-
-
-
-
-            
-            })
-
+        progressBar.setAttribute("value", Math.round(progress));
+        if (progress === 100) {
+          console.log("inside");
+          progressBar.style.display = "none";
+          uploadPrompt.innerHTML = "Upload Finished!";
+          uploadPrompt.style.display = "block";
+          setTimeout(function () {
+            uploadPrompt.innerHTML = "Generating embedding...";
+          }, 500);
         }
-        )
-    
-    }
+      },
+      function (error) {
+        console.log(error.message);
+      },
+      function () {
+        //succesful upload
+        uploadTask.snapshot.ref.getDownloadURL().then(function (downloadURL) {
+          //uploaded image url
+          console.log(downloadURL);
+          singleGlobalVar.downloadURL = downloadURL;
 
-    else{
-        document.getElementById("uploadDone").innerHTML = "Please add an image for upload";
-        document.getElementById("uploadDone").style.display="block";
-    
-    }
+          //calling cloud function and saving it in singleGlobalVar--------------------------------
+
+          var xhr = new XMLHttpRequest();
+          var url =
+            "https://us-central1-first-cloud-function-282616.cloudfunctions.net/face_embedding";
+          xhr.open("POST", url, true);
+          xhr.setRequestHeader("Content-Type", "application/json");
+          xhr.onreadystatechange = function () {
+            if ((xhr.readyState === 4) & (xhr.status === 200)) {
+              //var respond = JSON.parse(this.responseText);
+              var embed = JSON.parse(xhr.responseText);
+
+              singleGlobalVar.embedding = embed.embedding;
+              console.log(embed.embedding + "type: " + typeof embed.embedding);
+
+              setTimeout(function () {
+                uploadImage.style.display = "none";
+                userInfo.style.display = "block";
+                uploadPrompt.style.display = "none";
+                document.getElementById("heading").innerHTML =
+                  "Add Information related to Image ";
+              }, 500);
+              //singleGlobalVar.embedding = JSON.parse(xhr.responseText).embedding;
+              //alert(singleGlobalVar.embedding[0]);
+            }
+          };
+
+          var data = JSON.stringify({
+            downloadURL: singleGlobalVar.downloadURL,
+          });
+          xhr.send(data);
+        });
+      }
+    );
+  } else {
+    document.getElementById("uploadDone").innerHTML =
+      "Please add an image for upload";
+    document.getElementById("uploadDone").style.display = "block";
+  }
 }
 
 const inpFile = document.getElementById("inpFile");
 const previewContainer = document.getElementById("imagePreview");
 const previewImage = previewContainer.querySelector(".image-preview__image");
-const previewDefaultTex = previewContainer.querySelector(".image-preview__default-text");
+const previewDefaultTex = previewContainer.querySelector(
+  ".image-preview__default-text"
+);
 
-inpFile.addEventListener("change" , function() {
+inpFile.addEventListener("change", function () {
+  document.getElementById("uploadDone").style.display = "none";
+  document.getElementById("progressBar").setAttribute("value", "0");
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    previewDefaultTex.style.display = "none";
+    previewImage.style.display = "block";
 
-    document.getElementById("uploadDone").style.display="none";
-    document.getElementById("progressBar").setAttribute("value","0");
-    const file = this.files[0];
-    if(file) {
-        const reader = new FileReader();
-        previewDefaultTex.style.display="none";
-        previewImage.style.display="block";
+    reader.addEventListener("load", function () {
+      console.log(this);
+      previewImage.setAttribute("src", this.result);
+    });
 
-        reader.addEventListener("load", function() {
-            console.log(this);
-            previewImage.setAttribute("src", this.result);
-
-        });
-
-        reader.readAsDataURL(file);
-    }
-
-    else{
-            previewDefaultTex.style.display=null;
-            previewImage.style.display=null;
-            previewImage.setAttribute("src","");
-
-        }
-
-    
-})
-
+    reader.readAsDataURL(file);
+  } else {
+    previewDefaultTex.style.display = null;
+    previewImage.style.display = null;
+    previewImage.setAttribute("src", "");
+  }
+});
 
 function submitInfo() {
+  var name = document.getElementById("userName").value;
+  var relation = document.getElementById("userRelation").value;
+  var database = firebase.database();
+  userData = database.ref().child("users");
+  var user = firebase.auth().currentUser;
+  var userEmail = user.email;
+  var userID = user.uid;
 
-    var name = document.getElementById("userName").value;
-    var relation = document.getElementById("userRelation").value;
-    var database = firebase.database(); 
-    userData = database.ref().child("users");
-    var user = firebase.auth().currentUser;
-    var userEmail = user.email;
-    var userID = user.uid;
-    
-    console.log("works");
-    console.log(user);
+  console.log("works");
+  console.log(user);
 
+  if (name) {
+    console.log("got in");
+    console.log(typeof userEmail);
+    userData.child(userID).push({
+      name: name,
+      relationship: relation,
+      embedding: singleGlobalVar.embedding,
+      downloadURL: singleGlobalVar.downloadURL,
+    });
+  }
 
-
-    if(name)
-    {
-        console.log("got in");
-        console.log(typeof(userEmail));
-        userData.child(userID).push({
-            name: name,
-            relationship: relation,
-            embedding:singleGlobalVar.embedding,
-            downloadURL:singleGlobalVar.downloadURL
-
-        });
-    }
-
-
-    setTimeout( function () {
-        document.getElementById("submitInfo").style.display = "none";
-        document.getElementById("afterSubmit").style.display="block";
-        },1000)
-
+  setTimeout(function () {
+    document.getElementById("submitInfo").style.display = "none";
+    document.getElementById("afterSubmit").style.display = "block";
+  }, 1000);
 }
-
-    
-
-
-
-
-
-
-
-    
